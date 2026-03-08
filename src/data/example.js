70957516cd6d4fe02b531ea8b47b1ec9c6b28d13
Flagy52 <den.sharov.06@bk.ru>
2026-03-08 19:23:54 +0300

    Add images and data/projects

---
+++ src/data/example.js
@@ -0,0 +1,17 @@
+export const example = [
+  {
+    id: 1,
+    title: "title",
+    description: "desc",
+    fullDescription: "fulldesc",
+    colors: ["#013EFF", "#00DDFD", "#FFFFFF"],
+    technologies: ["JS"],
+    screenshots: [
+      "/img/exm/1"
+    ],
+    link: "/project/1",
+    linkDemo: "examlpeLink",
+    linkGithub: "examlpeLink",
+    linkFigma: "examlpeLink",
+  }
+] 
---
+++ src/data/projects.js
@@ -0,0 +1,129 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Студия Aura Sonic",
+    description: "Дизайн-концепт для студии записи подкастов",
+    fullDescription: `Разработка современного и атмосферного дизайна для студии звукозаписи Aura Sonic. 
+    Главная задача — создать интерфейс, который отражает профессиональную, 
+    но при этом уютную атмосферу студии. В работе сделан упор на тёмную тему, 
+    плавные градиенты и чёткую типографику, чтобы подчеркнуть премиальность бренда.`,
+    colors: ["#013EFF", "#00DDFD", "#FFFFFF"],
+    technologies: ["Figma", "UI/UX Design", "Концепт-дизайн"],
+    screenshots: [
+      "/img/project1/aura.png",
+      "/img/project1/2.png",
+      "/img/project1/3.png",
+      "/img/project1/4.png",
+      "/img/project1/5.png"
+    ],
+    link: "/project/1",
+    linkFigma: "https://www.figma.com/design/OqYrv8oYHu7VPgnmHY6eq0/Aura-sonic?node-id=0-1&t=A6wqJFQ0xyJnrM2K-1",
+  },
+  {
+    id: 2,
+    title: "Магазин Blade",
+    description: "Дизайн и верстка сайта для магазина катан",
+    fullDescription: `Полная разработка сайта для магазина японских катанов Blade. 
+    Спроектировал современный и атмосферный дизайн в Figma, затем сверстал его на чистом HTML и CSS. 
+    Визуальный стиль выполнен в минималистичном ключе с отсылками к японской эстетике: 
+    сдержанная цветовая гамма с акцентами красного, четкая типографика и продуманная структура карточек товаров. 
+    Сайт получился стильным, удобным и полностью передает дух бренда`,
+    colors: ["#000", "#FFFFFF" ],
+    technologies: ["Figma", "HTML", "CSS", "Адаптивная верстка", "php"],
+    screenshots: [
+      "/img/project2/blade.png",
+      "/img/project2/2.png",
+      "/img/project2/3.png",
+      "/img/project2/4.png",
+      "/img/project2/5.png",
+      "/img/project2/6.png",
+    ],
+    link: "/project/2",
+    linkFigma: "https://www.figma.com/design/Obb6AjMXBuW5jLelMvNQK6/blade?node-id=0-1&p=f&t=HouaYIDxupAo6zxw-0"
+  },
+  {
+    id: 3,
+    title: "Кулинарная академия MasterChef",
+    description: "Сайт академии с мероприятиями и курсами",
+    fullDescription: `Разработка дизайна и верстка лендинга для кулинарной академии MasterChef. 
+    Проект включает в себя презентацию курсов, галерею работ учеников и блоки с рецептами. 
+    Верстка выполнена с использованием препроцессора SASS, что обеспечило модульность и легкость поддержки кода. 
+    Дизайн получился ярким, «вкусным» и аппетитным, чтобы привлечь внимание посетителей.`,
+    colors: ["#2C3E50", "#FF6B35", "#FFFFFF"],
+    technologies: ["HTML", "SCSS", "Figma", "Адаптивная верстка"],
+    screenshots: [
+      "/img/project3/1.png",
+      "/img/project3/2.png",
+      "/img/project3/3.png",
+      "/img/project3/4.png",
+      "/img/project3/5.png",
+      "/img/project3/6.png",
+    ],
+    link: "/project/3",
+    linkDemo: "https://flagy52.github.io/masterChef/",
+    linkGithub: "https://github.com/Flagy52/masterChef",
+    linkFigma: "https://www.figma.com/design/eAkhCif9ifRxczVwdzvEn9/MasterChef?node-id=0-1&p=f&t=Y5FVTdlkBAwQl4p6-0"
+  },
+  {
+    id: 4,
+    title: "Брендбук Aura Sonic",
+    description: "Фирменный стиль и айдентика для студии подкастов",
+    fullDescription: `Разработка полноценного брендбука для студии Aura Sonic. 
+    Включает в себя логотип, фирменные цвета, типографику, 
+    паттерны и правила использования бренда на всех носителях — от соцсетей до печатной продукции. 
+    Создан единый визуальный язык, который передает атмосферу студии: современность, профессионализм и творческую энергию`,
+    colors: ["#013EFF", "#00DDFD"],
+    technologies: ["Figma", "Фирменный стиль"],
+    screenshots: [
+      "/img/project4/1.png",
+      "/img/project4/2.png",
+      "/img/project4/3.png",
+      "/img/project4/4.png",
+      "/img/project4/5.png",
+      "/img/project4/6.png",
+      "/img/project4/7.png",
+      "/img/project4/8.png",
+      "/img/project4/9.png",
+      "/img/project4/10.png",
+    ],
+    link: "/project/4",
+    linkFigma: 'https://www.figma.com/deck/GREQsuqo7JsDXCK3wjy9K0'
+  },
+  {
+    id: 5,
+    title: "Профориентационный бот в Telegram",
+    description: "Тест из 10 вопросов для определения IT-специальности",
+    fullDescription: `Разработал Telegram-бота на Node.js, который помогает новичкам выбрать направление в IT.
+    Бот задает 10 вопросов, анализирует ответы и рекомендует подходящую специальность 
+    (frontend, backend, дизайн, аналитика и т.д.). Реализована логика ветвления сценариев.`,
+    technologies: ["Node.js", "JavaScript", "Telegram API", "JSON"],
+    screenshots: [
+      "/img/project5/prev.png",
+      "/img/project5/1.png",
+      "/img/project5/2.png",
+      "/img/project5/3.png",
+      "/img/project5/4.png",
+      "/img/project5/5.png",
+      "/img/project5/6.png",
+    ],
+    link: "/project/5"
+  },
+  {
+    id: 6,
+    title: "Серия графических постеров",
+    description: "Коллекция плакатов в разных стилях",
+    fullDescription: `Серия авторских постеров, созданных в Figma. 
+    Каждая работа — это эксперимент с формой, цветом и композицией. 
+    В коллекции представлены постеры в разных стилях: от минимализма и геометрии до коллажей, 
+    неона и типографических экспериментов.`,
+    technologies: ["Figma", "Photoshop", "Illustrator"],
+    screenshots: [
+      "/img/project6/1.png",
+      "/img/project6/2.png",
+      "/img/project6/4.png",
+      "/img/project6/5.png",
+      "/img/project6/6.png",
+    ],
+    link: "/project/6"
+  }
+];
