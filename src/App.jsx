8134b7e9f02da11fe47f6ff9258035f7c6f46734
Flagy52 <den.sharov.06@bk.ru>
2026-03-17 09:42:13 +0300

    Fix delete

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Hero from './components/Hero';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import { NeuralNetworkBG } from 'jcodes-styles';
 import AboutPage from './pages/AboutPage';
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
